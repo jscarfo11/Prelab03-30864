c008dab92f8e97b8d255148c286cdaa7f25c7aee
Jack Scarfo <jackdscarfo@gmail.com>
2025-01-31 23:31:38 -0500

    MFLP-19 Start migration

---
+++ website/django.py
@@ -0,0 +1,15 @@
+
+
+class DjangoServer:
+    def __init__(self):
+        """Initialize the Django server."""
+        print("Django server initialized.")
+    """Server for the website."""
+    
+    def run(self):
+        """Run the Django server."""
+        print("Running Django server.")
+    
+    def create(self):
+        """Create the Django server."""
+        print("Creating Django server.")
--- website/main.py
+++ website/main.py
@@ -1,9 +1,14 @@
 
+from django_server import DjangoServer
 
 def main():
     """Server for the website."""
 
     # run a django server here
+
+    django_server = DjangoServer()
+    django_server.create()
+    django_server.run()
 
 
 if __name__ == "__main__":
